6f2152b326a940f8f97df1ad84dbd72629524980
Takumi <75904606+Omuretsu@users.noreply.github.com>
2026-06-02 21:59:30 +0900

    feat: MACDを取得する関数を別ファイルへ切り出し

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from macd import calc_macd, signal
 import yfinance as yf
 from datetime import datetime
 from pathlib import Path
@@ -18,16 +19,6 @@ def calc_rsi(df, period):
     rsi = 100 - (100 / (1 + rs))
 
     return rsi
-
-
-def calc_macd(df):
-    ema12 = df['Close'].ewm(span=12, adjust=False).mean()
-    ema26 = df['Close'].ewm(span=26, adjust=False).mean()
-
-    macd = ema12 - ema26
-    signal = macd.ewm(span=9, adjust=False).mean()
-
-    return macd, signal
 
 
 def calc_bollinger(df, window=25, sigma=3):

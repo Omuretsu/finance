a12d22c1a4e046bf1a7e3f526222a682b1f3e3c0
Takumi <75904606+Omuretsu@users.noreply.github.com>
2026-06-02 22:01:17 +0900

    feat: ボリンジャーバンドを取得する関数を別ファイルへ切り出し

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bollinger import calc_bollinger
 import yfinance as yf
 from datetime import datetime
 from pathlib import Path
@@ -16,21 +17,6 @@ def calc_macd(df):
     signal = macd.ewm(span=9, adjust=False).mean()
 
     return macd, signal
-
-
-def calc_bollinger(df, window=25, sigma=3):
-    bb_ma = df['Close'].rolling(window).mean()
-    bb_std = df['Close'].rolling(window).std()
-
-    upper1 = bb_ma + (1 * bb_std)
-    lower1 = bb_ma - (1 * bb_std)
-
-    upper2 = bb_ma + (2 * bb_std)
-    lower2 = bb_ma - (2 * bb_std)
-
-    upper3 = bb_ma + (3 * bb_std)
-    lower3 = bb_ma - (3 * bb_std)
-    return bb_ma, bb_std, upper1, lower1, upper2, lower2, upper3, lower3
 
 
 def percent(df):

59250890e6bc238967a377a9c5532a49f7b86e3d
Takumi <75904606+Omuretsu@users.noreply.github.com>
2026-06-02 22:46:47 +0900

    feat: 式の構造を変更

--- indicators/hlpercent.py
+++ indicators/hlpercent.py
@@ -1,6 +1,8 @@
 def hlpercent(df):
     df["HL_diff"] = df["High"] - df["Low"]
-    df["HL_pct"] = (df["High"] - df["Low"]) / df["Low"] * 100
-    df["body_pct"] = (df["Close"] - df["Open"]) / \
-        (df["High"] - df["Low"]) * 100
+    df["HL_pct"] = (df["High"] - df["Low"]) / df["Open"] * 100
+
+    df["body_pct"] = (
+        (df["Close"] - df["Open"]) /
+        (df["High"] - df["Low"])) * 100
     return df

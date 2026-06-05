9bd1ebcb7a5005e9d2cdbb3be02d0a8341aa4064
Takumi <75904606+Omuretsu@users.noreply.github.com>
2026-06-06 03:34:34 +0900

    feat: 各種計算機構の追加

--- data_loader.py
+++ data_loader.py
@@ -1,20 +1,26 @@
 import yfinance as yf
 
-from indicators.rename import rename_columns
 from indicators.rsi import calc_rsi
 from indicators.macd import calc_macd
 from indicators.bollinger import calc_bollinger
 from indicators.flpercent import flpercent
 from indicators.hlpercent import hlpercent
+from indicators.volume import Volume_ratio
 
 
 def get_stock_data(symbol):
     ticker = yf.Ticker(symbol)
 
-    df = ticker.history(period="1y")
+    df = ticker.history(
+        period="1y",
+        auto_adjust=False
+    )
+    df = df.dropna(subset=["Close"])
+
     df = flpercent(df)
     df = hlpercent(df)
 
+    df['Volume_Ratio'] = Volume_ratio(df)
     df['MA5'] = df['Close'].rolling(5).mean()
     df['MA25'] = df['Close'].rolling(25).mean()
     df['MA75'] = df['Close'].rolling(75).mean()
@@ -22,7 +28,7 @@ def get_stock_data(symbol):
     df['RSI7'] = calc_rsi(df, 7)
     df['RSI14'] = calc_rsi(df, 14)
     df['RSI28'] = calc_rsi(df, 28)
-    df['MACD'], df['Signal'] = calc_macd(df)
+    df['MACD'], df['Signal'], df['MACD_Gap'] = calc_macd(df)
 
     bb_ma, bb_std, upper1, lower1, upper2, lower2, upper3, lower3 = calc_bollinger(
         df, window=25, sigma=3)
